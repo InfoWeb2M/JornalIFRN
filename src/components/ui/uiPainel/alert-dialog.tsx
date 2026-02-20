ebe5936b0b2bd4ac0c63df6df9507125c9bb3075
Benin10-bit <beniciodoamaralneto2019@gmail.com>
2026-02-20 16:22:03 -0300

    desisti de descrever o commit

--- src/components/ui/uiPainel/alert-dialog.tsx
+++ src/components/ui/uiPainel/alert-dialog.tsx
@@ -2,7 +2,7 @@ import * as React from "react";
 import * as AlertDialogPrimitive from "@radix-ui/react-alert-dialog";
 
 import { cn } from "@/lib/utils";
-import { buttonVariants } from "@/components/uiPainel/button";
+import { buttonVariants } from "@/components/ui/uiPainel/button";
 
 const AlertDialog = AlertDialogPrimitive.Root;
 
