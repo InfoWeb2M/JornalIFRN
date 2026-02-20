3da8d5157b13670569ed81f1b67947f23d940c8e
Benin10-bit <beniciodoamaralneto2019@gmail.com>
2026-02-20 15:35:33 -0300

    ajuste do cache da aplicação

--- src/actions/CatalogActions/catalogActions.ts
+++ src/actions/CatalogActions/catalogActions.ts
@@ -1,11 +1,12 @@
 import { API_MAIN_ROUTE } from "@/constants/apiRoute";
+import { cacheLife } from "next/cache";
 
 async function catalogActions() {
   //Pode comentar o use cache e cache life em ambiente de teste
 
-  /*   "use cache"
+  "use cache"
 
-  cacheLife("days") */
+  cacheLife("days")
 
   const response = await fetch(API_MAIN_ROUTE + "/news/show-news");
 
