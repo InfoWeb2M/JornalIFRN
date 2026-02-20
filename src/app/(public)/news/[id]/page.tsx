5df39a0e4134a5326059a99c093d48515b7099f7
Benin10-bit <beniciodoamaralneto2019@gmail.com>
2026-02-20 20:39:00 -0300

    hgHG

--- src/app/(public)/news/[id]/page.tsx
+++ src/app/(public)/news/[id]/page.tsx
@@ -2,22 +2,21 @@ import { searchNewsActions } from "@/actions/SearchNews/searchNewsActions";
 import NewsDetail from "@/components/newsPage/page";
 import Footer from "@/components/ui/footer/page";
 import Header from "@/components/ui/header/page";
-import { Suspense } from "react";
 
 export const dynamic = "force-dynamic";
-export default async function News({ params }: { params: Promise<{ id: string }> }) {
-    const resolvedParams = await params;
 
-    const noticia = searchNewsActions(resolvedParams.id);
+export default async function News({
+  params,
+}: {
+  params: { id: string };
+}) {
+  const noticia = await searchNewsActions(params.id);
 
-    return (
-        <div>
-            <Suspense fallback={<span className="text-center text-(--text)">carregando noticia...</span>}>
-                <NewsDetail news={await noticia} />
-            </Suspense>
-            <Suspense>
-                <Footer />
-            </Suspense>
-        </div>
-    );
+  return (
+    <div>
+      <Header />
+      <NewsDetail news={noticia} />
+      <Footer />
+    </div>
+  );
 }
